4e0815e680319db76e57066adafbcad4a1d149fd
JohanRodin <rodinjohan@gmail.com>
2017-05-02 09:11:07 +0200

    cleaned

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-var connect = require('connect');<br/>var serveStatic = require('serve-static');<br/>var port = (process.env.VCAP_APP_PORT || 3000);<br/>var host = (process.env.VCAP_APP_HOST || 'localhost');<br/>connect().use(serveStatic(__dirname)).listen(port,host);<br/>
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+connect().use(serveStatic(__dirname)).listen(port,host);
