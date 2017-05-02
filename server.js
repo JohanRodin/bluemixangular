887a4ad6075dabf826878dcf0785f28a12a49040
JohanRodin <rodinjohan@gmail.com>
2017-05-02 08:57:27 +0200

    Created.

---
+++ server.js
@@ -0,0 +1,1 @@
+var connect = require('connect');<br/>var serveStatic = require('serve-static');<br/>var port = (process.env.VCAP_APP_PORT || 3000);<br/>var host = (process.env.VCAP_APP_HOST || 'localhost');<br/>connect().use(serveStatic(__dirname)).listen(port,host);<br/>
