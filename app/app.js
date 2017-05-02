b0e31468f5a71a84ee330dbf9f8a9c3f780adcf7
JohanRodin <rodinjohan@gmail.com>
2017-05-02 12:03:29 +0200

    marks

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
 app.config(['$myController', function($myController) {
         $myController.allowGlobals();
     }]);
