61e4ed4a68fc054edec777f0a29f04ee3cbecc55
JohanRodin <rodinjohan@gmail.com>
2017-05-02 09:31:22 +0200

    cleaned.

--- app/myController.js
+++ app/myController.js
@@ -1,1 +1,7 @@
-app.controller('myController',['$scope', function($scope){<br/>$scope.name= "My First Angular App";<br/>$scope.version = "v1.0.1";<br/>$scope.author = "Created by &lt;add your name&gt;";<br/>$scope.release ="Released on &lt;add a date&gt;";<br/>$scope.message ="This app is &lt;add your message&gt;";<br/>}]);<br/>
+app.controller('myController',['$scope', function($scope){
+  $scope.name= "My First Angular App";
+  $scope.version = "v1.0.1";
+  $scope.author = "Created by <Johan Rodin>";
+  $scope.release ="Released on <May 2nd 2017>";
+  $scope.message ="This app is <a demo of angular on Bluemix>";
+}]);
