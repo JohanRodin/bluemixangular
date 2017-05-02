04ae7f70e709d5588f32492eb4bd18688946024b
JohanRodin <rodinjohan@gmail.com>
2017-05-02 11:42:44 +0200

    allow globals

--- app/app.js
+++ app/app.js
@@ -1,1 +1,4 @@
 var app = angular.module("myApp", ['ngRoute']).controller('myController', myController);
+app.config(['$myControll', function($myControll) {
+        $myControll.allowGlobals();
+    }]);
